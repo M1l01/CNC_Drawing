5bd3b5266ee55ff6f0bd286f69acf2a347fe0516
M1l01 <emiliolemos2001@hotmail.com>
2025-08-06 16:50:12 -0500

    Feat: Measuring caracteristics of motor pap

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,17 @@
 #define Module_IN3 4
 #define Module_IN4 5
 
+int cont_step = 0;
+int steps_per_rev = 2064;
 int step = 0;
 
 // Function prototype
 void OneStep(bool dir);
 
 void setup() {
+    // Initialize serial communication
+    Serial.begin(9600);
+
     pinMode(Module_IN1, OUTPUT);
     pinMode(Module_IN2, OUTPUT);
     pinMode(Module_IN3, OUTPUT);
@@ -18,73 +23,55 @@ void setup() {
 }
 
 void loop() {
-    OneStep(false);
-    delay(2);
+    if (cont_step <= steps_per_rev) {
+      OneStep(true); // Rotate clockwise
+      cont_step++;
+      delay(2);
+    }
 }
 
 // Functions
 void OneStep(bool dir) {
   if (dir) {
-    switch (step){
-    case 0:
-      digitalWrite(Module_IN1, HIGH);
-      digitalWrite(Module_IN2, LOW);
-      digitalWrite(Module_IN3, LOW);
-      digitalWrite(Module_IN4, LOW);
-      break;
-    case 1:
-      digitalWrite(Module_IN1, LOW);
-      digitalWrite(Module_IN2, HIGH);
-      digitalWrite(Module_IN3, LOW);
-      digitalWrite(Module_IN4, LOW);
-      break;
-    case 2:
-      digitalWrite(Module_IN1, LOW);
-      digitalWrite(Module_IN2, LOW);
-      digitalWrite(Module_IN3, HIGH);
-      digitalWrite(Module_IN4, LOW);
-      break;
-    case 3:
-      digitalWrite(Module_IN1, LOW);
-      digitalWrite(Module_IN2, LOW);
-      digitalWrite(Module_IN3, LOW);
-      digitalWrite(Module_IN4, HIGH);
-      break;
-    default:
-      break;
+    // Advance step
+    step++;
+    if (step > 3) {
+      step = 0;
     }
-  }else{
-    switch (step) {
-    case 0:
-      digitalWrite(Module_IN1, LOW);
-      digitalWrite(Module_IN2, LOW);
-      digitalWrite(Module_IN3, LOW);
-      digitalWrite(Module_IN4, HIGH);
-      break;
-    case 1:
-      digitalWrite(Module_IN1, LOW);
-      digitalWrite(Module_IN2, LOW);
-      digitalWrite(Module_IN3, HIGH);
-      digitalWrite(Module_IN4, LOW);
-      break;
-    case 2:
-      digitalWrite(Module_IN1, LOW);
-      digitalWrite(Module_IN2, HIGH);
-      digitalWrite(Module_IN3, LOW);
-      digitalWrite(Module_IN4, LOW);
-      break;
-    case 3:
-      digitalWrite(Module_IN1, HIGH);
-      digitalWrite(Module_IN2, LOW);
-      digitalWrite(Module_IN3, LOW);
-      digitalWrite(Module_IN4, LOW);
-      break;
-    default:
-      break;
+  }else {
+    // Reverse step
+    step--;
+    if (step < 0) {
+      step = 3;
     }
   }
-  step++;
-  if (step > 3) {
-    step = 0;
+
+  switch (step) {
+    case 0:
+      digitalWrite(Module_IN1, HIGH);
+      digitalWrite(Module_IN2, LOW);
+      digitalWrite(Module_IN3, LOW);
+      digitalWrite(Module_IN4, LOW);
+      break;
+    case 1:
+      digitalWrite(Module_IN1, LOW);
+      digitalWrite(Module_IN2, HIGH);
+      digitalWrite(Module_IN3, LOW);
+      digitalWrite(Module_IN4, LOW);
+      break;
+    case 2:
+      digitalWrite(Module_IN1, LOW);
+      digitalWrite(Module_IN2, LOW);
+      digitalWrite(Module_IN3, HIGH);
+      digitalWrite(Module_IN4, LOW);
+      break;
+    case 3:
+      digitalWrite(Module_IN1, LOW);
+      digitalWrite(Module_IN2, LOW);
+      digitalWrite(Module_IN3, LOW);
+      digitalWrite(Module_IN4, HIGH);
+      break;
+    default:
+      break;
   }
 }
