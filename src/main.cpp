ad7fddae030b159890eab7e51db3cd732555d8d7
M1l01 <emiliolemos2001@hotmail.com>
2025-08-09 10:51:16 -0500

    Feat: Adding button interrupts to control system on/off and direction CW/CCW

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,29 @@
 #include <Arduino.h>
 
 // Motor 1 control pins
-#define Module1_IN1 2
-#define Module1_IN2 3
-#define Module1_IN3 4
-#define Module1_IN4 5
+#define Module1_IN1 4
+#define Module1_IN2 5
+#define Module1_IN3 6
+#define Module1_IN4 7
 
 // Motor 2 control pins
-#define Module2_IN1 6
-#define Module2_IN2 7
-#define Module2_IN3 8
-#define Module2_IN4 9
+#define Module2_IN1 8
+#define Module2_IN2 9
+#define Module2_IN3 10
+#define Module2_IN4 11
 
-int cont_step = 0;
-int steps_per_rev = 2064;
+// Control de Sentido
+#define BTN_DIR 2
+#define BTN_ON_OFF 3
+
 int step = 0;
+bool system_state = false; // System state: off
+bool direction_state = false; // Direction: CCW
 
 // Function prototype
 void OneStep(bool dir, int IN1, int IN2, int IN3, int IN4);
+void direction_callback(void);
+void on_off_callback(void);
 
 void setup() {
     // Initialize serial communication
@@ -32,18 +38,37 @@ void setup() {
     pinMode(Module2_IN2, OUTPUT);
     pinMode(Module2_IN3, OUTPUT);
     pinMode(Module2_IN4, OUTPUT);
+
+    pinMode(BTN_DIR, INPUT);
+    pinMode(BTN_ON_OFF, INPUT);
+
+    // Configuration of interrupts pins
+    attachInterrupt(digitalPinToInterrupt(BTN_DIR), direction_callback, RISING);
+    attachInterrupt(digitalPinToInterrupt(BTN_ON_OFF), on_off_callback, RISING);
 }
 
 void loop() {
-    if (cont_step <= steps_per_rev) {
-      //OneStep(true, Module1_IN1, Module1_IN2, Module1_IN3, Module1_IN4); // Motor 1 Rotate clockwise
-      OneStep(true, Module2_IN1, Module2_IN2, Module2_IN3, Module2_IN4); // Motor 2 Rotate clockwise
-      cont_step++;
-      delay(3);
-    }
+  Serial.print("System State: ");
+  Serial.print(system_state ? "ON  |" : "OFF  |");
+  Serial.print(" Direction: ");
+  Serial.println(direction_state ? "CW" : "CCW");
+  delay(20);
 }
 
 // Functions
+// Callback function to change the state of the system (on/off)
+void on_off_callback(void) {
+  delay(20); // Debounce delay
+  system_state = !system_state; // Toggle system state
+}
+
+// Callback function to change the direction of the motor
+void direction_callback(void) {
+  delay(20); // Debounce delay
+  direction_state = !direction_state; // Toggle direction state
+}
+
+// Function to perform one step of the stepper motor
 void OneStep(bool dir, int IN1, int IN2, int IN3, int IN4) {
   if (dir) {
     // Advance step
